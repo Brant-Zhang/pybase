323d1dd99ab356e99ff45ae8e0333d1b35d5ec1e
zhanglei <zhanglei@supercxp.com>
2021-05-28 13:41:27 +0800

    add

--- tools/excel.py
+++ tools/excel.py
@@ -16,15 +16,17 @@ def allwork():
                 ex=excel(realname)
                 ex.prepare()
                 ex.cal()
-                oot.writeResult(filename,ex.output)
+                oot.saveResult(filename,ex.output)
     oot.writeDisk()
 
 class disk:
     def __init__(self):
-        self.dstname= dirname+"/result.xlsx"
-        self.buffer=pd.DataFrame([])
+        self.dstname = dirname+"/result.xlsx"
+        self.buffer = pd.DataFrame([])
+        if os.path.exists(self.dstname):
+            os.remove(self.dstname)
 
-    def writeResult(self,fname,nlist):
+    def saveResult(self,fname,nlist):
         xv=pd.DataFrame(nlist,columns=['分类','支出'])
         #添加新列，标记来自哪个文件
         xv['from']=fname
@@ -33,7 +35,7 @@ class disk:
 
     def writeDisk(self):
         with pd.ExcelWriter(self.dstname) as writer:
-            self.buffer.to_excel(writer,sheet_name='Sheet1')
+            self.buffer.to_excel(writer,sheet_name='Sheet1',index=False)
 
 class excel:
     def __init__(self,name):
@@ -56,7 +58,8 @@ class excel:
     def cal(self):
         data = pd.read_excel(self.name,sheet_name= 0,index_col=0,header=0)
         for item in self.paytypes:
-            targets=data[data["备注"]==item].iloc[:,[1,2]]
+            #targets=data[data["备注"]==item].iloc[:,[1,2]]
+            targets=data[data["备注"]==item].loc[:,['支出']]
             result=targets["支出"].sum()
             #print ("对于分类:",item, "总支出为",result)
             self.output.append([item,result])
@@ -66,3 +69,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#参考信息
+#https://www.jianshu.com/p/c3c2ac84fb02
+#https://pandas.pydata.org/pandas-docs/stable/reference/io.html
